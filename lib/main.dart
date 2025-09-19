4c4eea1b39955e436218b6be73e33382d8be1876
riccardo.tralli <riccardo.tralli@live.it>
2025-09-19 16:51:41 +0200

    feat: configure ChangeCase for special character handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:change_case/change_case.dart';
 import 'package:fidely_app/app.dart';
 import 'package:fidely_app/services/loyalty_card_service.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await LoyaltyCardService.instance.init();
+
+  // Configure ChangeCase to handle special characters (accents)
+  ChangeCaseConfig.setUp(
+    stripPatterns: [RegExp("[^A-Z0-9À-ÿ]+", caseSensitive: false)],
+  );
 
   runApp(const App());
 }
