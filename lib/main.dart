ee0c09072da16e9bfee42cfcfd76e4be085fe918
riccardo.tralli <riccardo.tralli@live.it>
2025-09-19 16:54:28 +0200

    feat: enforce device orientation for better display

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:change_case/change_case.dart';
 import 'package:fidely_app/app.dart';
 import 'package:fidely_app/services/loyalty_card_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +13,11 @@ void main() async {
   ChangeCaseConfig.setUp(
     stripPatterns: [RegExp("[^A-Z0-9À-ÿ]+", caseSensitive: false)],
   );
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   runApp(const App());
 }
