6abff3acb16aa96a28e9e436a046cb884bf0d447
riccardo.tralli <riccardo.tralli@live.it>
2025-11-09 16:39:37 +0100

    feat: enhance Hicon widget with background color and padding support

--- lib/widgets/hicon.dart
+++ lib/widgets/hicon.dart
@@ -1,14 +1,31 @@
+import 'package:fidely_app/misc/themes/rradius.dart';
+import 'package:fidely_app/misc/themes/spaces.dart';
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
 
 class Hicon extends StatelessWidget {
   final List<List<dynamic>> icon;
   final Color? color;
+  final Color? backgroundColor;
   final double? size;
 
-  const Hicon(this.icon, {super.key, this.color, this.size});
+  const Hicon(
+    this.icon, {
+    super.key,
+    this.color,
+    this.backgroundColor,
+    this.size,
+  });
 
   @override
-  Widget build(BuildContext context) =>
-      HugeIcon(icon: icon, color: color, size: size ?? 24);
+  Widget build(BuildContext context) => Container(
+    padding: backgroundColor != null ? EdgeInsets.all(Spaces.extraSmall) : null,
+    decoration: BoxDecoration(
+      color: backgroundColor,
+      borderRadius: backgroundColor != null
+          ? BorderRadius.circular(RRadius.small)
+          : null,
+    ),
+    child: HugeIcon(icon: icon, color: color, size: size ?? 24),
+  );
 }
