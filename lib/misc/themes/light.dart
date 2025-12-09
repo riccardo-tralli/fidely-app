537b7d6dbafc607c65ecb66e9329867f787d3ba7
Riccardo Tralli <riccardo.tralli@live.it>
2025-12-09 19:41:18 +0100

    feat: update light theme for improved UI consistency

--- lib/misc/themes/light.dart
+++ lib/misc/themes/light.dart
@@ -1,17 +1,26 @@
+import 'package:fidely_app/misc/themes/fonts.dart';
+import 'package:fidely_app/misc/themes/rradius.dart';
 import 'package:flutter/material.dart';
 
 class LightTheme {
-  static final primaryColor = Colors.orange;
-  static final secondaryColor = Colors.lightGreen.shade100;
-  static final backgroundColor = Colors.grey.shade100;
-  static final onBackgroundColor = Colors.grey.shade200;
-  static final surfaceColor = Colors.white;
-  static final onSurfaceColor = Colors.black87;
-  static final snackBarColor = Colors.grey.shade800;
-  static final snackBarTextColor = Colors.white;
+  static final Color primaryColor = const Color.fromARGB(255, 36, 123, 194);
+  static final Color secondaryColor = Color.fromARGB(255, 255, 173, 51);
 
-  static final textColor = Colors.black;
-  static final errorColor = Colors.red.shade300;
+  static final Color backgroundColor = Colors.white;
+  static final Color onBackgroundColor = Colors.grey.shade200;
+  static final Color surfaceColor = Colors.white;
+  static final Color onSurfaceColor = Colors.black87;
+
+  static final Color snackBarColor = Colors.grey.shade800;
+  static final Color snackBarTextColor = Colors.white;
+
+  static final Color inputSurfaceColor = Colors.grey.shade100;
+  static final Color inputBorderColor = Colors.grey.shade200;
+
+  static final Color textColor = Colors.grey.shade900;
+  static final Color errorColor = Colors.red.shade300;
+
+  static final double textInitialSize = 16;
 
   static ThemeData make() => ThemeData(
     primaryColor: primaryColor,
@@ -25,30 +34,56 @@ class LightTheme {
       shadow: Colors.black.withAlpha(100),
     ),
     textTheme: TextTheme(
-      bodyLarge: TextStyle(color: textColor),
-      bodyMedium: TextStyle(color: textColor),
-      bodySmall: TextStyle(color: textColor),
-      titleLarge: TextStyle(color: textColor),
-      titleMedium: TextStyle(color: textColor),
-      titleSmall: TextStyle(color: textColor),
+      bodyLarge: TextStyle(
+        color: textColor,
+        fontFamily: Fonts.epilogue,
+        fontSize: textInitialSize * 1.2,
+      ),
+      bodyMedium: TextStyle(
+        color: textColor,
+        fontFamily: Fonts.epilogue,
+        fontSize: textInitialSize,
+      ),
+      bodySmall: TextStyle(
+        color: textColor,
+        fontFamily: Fonts.epilogue,
+        fontSize: textInitialSize * 0.8,
+      ),
+      titleLarge: TextStyle(
+        color: textColor,
+        fontFamily: Fonts.leckerliOne,
+        fontSize: textInitialSize * 2.5,
+      ),
+      titleMedium: TextStyle(
+        color: textColor,
+        fontFamily: Fonts.epilogue,
+        fontSize: textInitialSize * 1.8,
+        fontWeight: FontWeight.bold,
+      ),
+      titleSmall: TextStyle(
+        color: textColor,
+        fontFamily: Fonts.epilogue,
+        fontSize: textInitialSize * 1.6,
+      ),
     ),
+    iconTheme: IconThemeData(color: textColor, size: textInitialSize),
     inputDecorationTheme: InputDecorationTheme(
       filled: true,
-      fillColor: surfaceColor,
+      fillColor: inputSurfaceColor,
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: BorderSide(color: onBackgroundColor),
+        borderRadius: BorderRadius.circular(RRadius.medium),
+        borderSide: BorderSide(color: inputBorderColor),
       ),
       enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
-        borderSide: BorderSide(color: onBackgroundColor),
+        borderRadius: BorderRadius.circular(RRadius.medium),
+        borderSide: BorderSide(color: inputBorderColor),
       ),
       focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
+        borderRadius: BorderRadius.circular(RRadius.medium),
         borderSide: BorderSide(color: primaryColor),
       ),
       errorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(16),
+        borderRadius: BorderRadius.circular(RRadius.medium),
         borderSide: BorderSide(color: errorColor),
       ),
     ),
@@ -56,7 +91,15 @@ class LightTheme {
       style: ButtonStyle(
         minimumSize: WidgetStatePropertyAll(Size(0, 48)),
         shape: WidgetStatePropertyAll(
-          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(RRadius.medium),
+          ),
+        ),
+        textStyle: WidgetStatePropertyAll(
+          TextStyle(
+            fontFamily: Fonts.epilogue,
+            fontSize: textInitialSize * 1.2,
+          ),
         ),
       ),
     ),
@@ -65,8 +108,8 @@ class LightTheme {
       contentTextStyle: TextStyle(color: snackBarTextColor, fontSize: 16),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(16),
-          topRight: Radius.circular(16),
+          topLeft: Radius.circular(RRadius.medium),
+          topRight: Radius.circular(RRadius.medium),
         ),
       ),
     ),
