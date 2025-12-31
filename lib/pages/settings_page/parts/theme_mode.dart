1f56ad7c611b645761feed5ce03515f3eb97ca9d
Riccardo Tralli <riccardo.tralli@live.it>
2025-12-31 16:07:17 +0100

    refactor: fully modularize settings_page file

---
+++ lib/pages/settings_page/parts/theme_mode.dart
@@ -0,0 +1,52 @@
+part of "../settings_page.dart";
+
+Widget themeMode({required BuildContext context}) =>
+    BlocBuilder<DarkModeCubit, ThemeMode>(
+      builder: (context, state) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            L10n.of(context)!.settings_page_theme_title,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          Text(L10n.of(context)!.settings_page_theme_description),
+          SizedBox(height: Spaces.small),
+          IntrinsicHeight(
+            child: Row(
+              spacing: Spaces.medium,
+              children: [light(context), dark(context), system(context)],
+            ),
+          ),
+        ],
+      ),
+    );
+
+Widget light(BuildContext context) => Expanded(
+  child: Option(
+    context: context,
+    icon: HugeIcons.strokeRoundedSun02,
+    label: L10n.of(context)!.settings_page_theme_mode_light,
+    onTap: () => context.read<DarkModeCubit>().set(ThemeMode.light),
+    active: context.read<DarkModeCubit>().state == ThemeMode.light,
+  ),
+);
+
+Widget dark(BuildContext context) => Expanded(
+  child: Option(
+    context: context,
+    icon: HugeIcons.strokeRoundedMoon01,
+    label: L10n.of(context)!.settings_page_theme_mode_dark,
+    onTap: () => context.read<DarkModeCubit>().set(ThemeMode.dark),
+    active: context.read<DarkModeCubit>().state == ThemeMode.dark,
+  ),
+);
+
+Widget system(BuildContext context) => Expanded(
+  child: Option(
+    context: context,
+    icon: HugeIcons.strokeRoundedSmartPhone02,
+    label: L10n.of(context)!.settings_page_theme_mode_system,
+    onTap: () => context.read<DarkModeCubit>().set(ThemeMode.system),
+    active: context.read<DarkModeCubit>().state == ThemeMode.system,
+  ),
+);
--- lib/pages/settings_page/settings_page.dart
+++ lib/pages/settings_page/settings_page.dart
@@ -12,8 +12,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hugeicons/hugeicons.dart';
 
+part "parts/theme_mode.dart";
 part "parts/view_mode.dart";
 part "parts/sort_mode.dart";
+part "parts/language.dart";
 
 class SettingsPage extends StatefulWidget {
   static const route = "/settings";
@@ -25,23 +27,6 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  late ThemeMode _themeMode;
-  late String? _languageCode;
-
-  void onLanguageChange(String? languageCode) {
-    setState(() {
-      _languageCode = languageCode;
-    });
-    context.read<LanguageCubit>().set(languageCode);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _themeMode = context.read<DarkModeCubit>().state;
-    _languageCode = context.read<LanguageCubit>().state;
-  }
-
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: TopBar(),
@@ -51,119 +36,13 @@ class _SettingsPageState extends State<SettingsPage> {
         child: Column(
           spacing: Spaces.medium,
           children: [
-            themeModeOption(context),
+            themeMode(context: context),
             viewMode(context: context),
             sortMode(context: context),
-            languageOption(context),
+            language(context: context),
           ],
         ),
       ),
     ),
-  );
-
-  Widget themeModeOption(BuildContext context) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        L10n.of(context)!.settings_page_theme_title,
-        style: Theme.of(context).textTheme.headlineSmall,
-      ),
-      Text(L10n.of(context)!.settings_page_theme_description),
-      SizedBox(height: Spaces.small),
-      IntrinsicHeight(
-        child: Row(
-          spacing: Spaces.medium,
-          children: [
-            Expanded(
-              child: Option(
-                context: context,
-                icon: HugeIcons.strokeRoundedSun02,
-                label: L10n.of(context)!.settings_page_theme_mode_light,
-                onTap: () {
-                  setState(() {
-                    _themeMode = ThemeMode.light;
-                  });
-                  context.read<DarkModeCubit>().set(ThemeMode.light);
-                },
-                active: _themeMode == ThemeMode.light,
-              ),
-            ),
-            Expanded(
-              child: Option(
-                context: context,
-                icon: HugeIcons.strokeRoundedMoon01,
-                label: L10n.of(context)!.settings_page_theme_mode_dark,
-                onTap: () {
-                  setState(() {
-                    _themeMode = ThemeMode.dark;
-                  });
-                  context.read<DarkModeCubit>().set(ThemeMode.dark);
-                },
-                active: _themeMode == ThemeMode.dark,
-              ),
-            ),
-            Expanded(
-              child: Option(
-                context: context,
-                icon: HugeIcons.strokeRoundedSmartPhone02,
-                label: L10n.of(context)!.settings_page_theme_mode_system,
-                onTap: () {
-                  setState(() {
-                    _themeMode = ThemeMode.system;
-                  });
-                  context.read<DarkModeCubit>().set(ThemeMode.system);
-                },
-                active: _themeMode == ThemeMode.system,
-              ),
-            ),
-          ],
-        ),
-      ),
-    ],
-  );
-
-  Widget languageOption(BuildContext context) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        L10n.of(context)!.settings_page_language_title,
-        style: Theme.of(context).textTheme.headlineSmall,
-      ),
-      Text(L10n.of(context)!.settings_page_language_description),
-      SizedBox(height: Spaces.small),
-      DropdownButtonFormField(
-        initialValue: _languageCode,
-        items: [
-          DropdownMenuItem(
-            value: null,
-            child: Text(L10n.of(context)!.settings_page_language_option_system),
-          ),
-          DropdownMenuItem(
-            value: "en",
-            child: Text(
-              L10n.of(context)!.settings_page_language_option_english,
-            ),
-          ),
-          DropdownMenuItem(
-            value: "fr",
-            child: Text(L10n.of(context)!.settings_page_language_option_french),
-          ),
-          DropdownMenuItem(
-            value: "es",
-            child: Text(
-              L10n.of(context)!.settings_page_language_option_spanish,
-            ),
-          ),
-          DropdownMenuItem(
-            value: "it",
-            child: Text(
-              L10n.of(context)!.settings_page_language_option_italian,
-            ),
-          ),
-        ],
-        onChanged: (value) => onLanguageChange(value),
-        style: Theme.of(context).textTheme.bodyLarge,
-      ),
-    ],
   );
 }
